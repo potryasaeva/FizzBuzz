5468944eecfe0c7b532c343e8de48c1012c6295b
Lidiia <l.potryasaeva@gmail.com>
2021-10-11 10:47:50 +0300

    fizzbuzz task

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbersArray = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                numbersArray[i] = i + 1;
+            }
+
+            foreach (int i in numbersArray)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                { Console.WriteLine(i); }
+
+            }
+
+        }
+    }
+}
